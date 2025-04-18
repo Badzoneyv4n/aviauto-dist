1c4e1f1fe9f70ae40884c1db9da9aa35876917b3
Badzoneyv4n <ndiyvan.rw@gmail.com>
2025-04-18 15:47:19 +0200

    Initial release of AviAuto bundle

---
+++ dist/aviauto.bundle.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         AviAuto
+// @namespace    http://tampermonkey.net/
+// @version      v-1.0
+// @description  AviAuto is an intelligent, modular Tampermonkey automation script designed to interact with the Aviator game across multiple betting platforms. It automates tasks like claiming rain (free bets), placing wagers, reloading the game, and sending notifications — all of which are controlled remotely via Firebase.
+// @author       BadZoneYv4n
+// @match        https://aviator-next.spribegaming.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript== 
+(()=>{})();
